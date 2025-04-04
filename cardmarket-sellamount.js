1b914400187c03c298ae2772894a076be3fd3663
p4blo4p <87369479+p4blo4p@users.noreply.github.com>
2025-04-04 12:35:56 +0200

    Update cardmarket-sellamount.js

--- cardmarket-sellamount.js
+++ cardmarket-sellamount.js
@@ -4,9 +4,10 @@
 // @match       https://www.cardmarket.com/*/Magic/Cards/*
 // @match       https://www.cardmarket.com/*/Magic/*/*/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      -
-// @description 13/2/2025, 17:21:18
+// @description Ensures sell-count elements are visible and styled appropriately.
+// @run-at      document-idle
 // ==/UserScript==
 
 (function() {
@@ -15,31 +16,32 @@
     function modifySellCountElements() {
         const elements = document.querySelectorAll('.sell-count');
         elements.forEach(element => {
-            element.classList.remove('d-none');
-            const newText = element.getAttribute('data-bs-original-title');
-            if (newText) {
-                const textToAppend = newText.match(/\|\s*(\d+)/);
-                if (textToAppend) {
-                    const newSpan = document.createElement('span');
-                    newSpan.textContent = "  | " + textToAppend[1];
-                    const numberLength = textToAppend[1].length;
-                    newSpan.style.color = numberLength >= 5 ? 'lime' : numberLength === 4 ? 'orange' : 'gray';
-                    element.appendChild(newSpan);
+            if (!element.classList.contains('modified')) {
+                element.classList.remove('d-none');
+                const newText = element.getAttribute('data-bs-original-title');
+                if (newText) {
+                    const textToAppend = newText.match(/\|\s*(\d+)/);
+                    if (textToAppend) {
+                        const newSpan = document.createElement('span');
+                        newSpan.textContent = "  | " + textToAppend[1];
+                        const numberLength = textToAppend[1].length;
+                        newSpan.style.color = numberLength >= 5 ? 'lime' : numberLength === 4 ? 'orange' : 'gray';
+                        element.appendChild(newSpan);
+                    }
                 }
+                element.style.width = 'auto';
+                element.classList.add('modified'); // Mark the element as modified
             }
-            element.style.width = 'auto';
         });
     }
 
     const observer = new MutationObserver((mutations) => {
         if (document.querySelectorAll('.sell-count').length > 0) {
             modifySellCountElements();
-            observer.disconnect();
         }
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
 
-    // Remove the DOMContentLoaded event listener
-    // window.addEventListener('DOMContentLoaded', modifySellCountElements);
+    window.addEventListener('load', modifySellCountElements); // Ensure the function runs on page load
 })();
