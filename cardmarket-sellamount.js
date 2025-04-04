cb908d321f0663ae1c85db020a7551000d83779b
p4blo4p <87369479+p4blo4p@users.noreply.github.com>
2025-04-04 14:43:36 +0200

    Update cardmarket-sellamount.js

--- cardmarket-sellamount.js
+++ cardmarket-sellamount.js
@@ -4,7 +4,7 @@
 // @match       https://www.cardmarket.com/*/Magic/Cards/*
 // @match       https://www.cardmarket.com/*/Magic/*/*/*
 // @grant       none
-// @version     1.41
+// @version     1.42
 // @description Ensures sell-count elements are visible and styled appropriately.
 // @run-at      document-idle
 // ==/UserScript==
@@ -17,29 +17,36 @@
     function modifySellCountElements() {
         const elements = document.querySelectorAll('.sell-count');
         console.log('Found elements:', elements.length);
+
         elements.forEach(element => {
-            if (!element.classList.contains('modified')) {
-                element.classList.remove('d-none');
-                const newText = element.getAttribute('data-bs-original-title');
-                console.log('Processing element:', newText);
-                if (newText) {
-                    const textToAppend = newText.match(/\|\s*(\d+)/);
-                    if (textToAppend) {
-                        const newSpan = document.createElement('span');
-                        newSpan.textContent = "  | " + textToAppend[1];
-                        const numberLength = textToAppend[1].length;
-                        newSpan.style.color = numberLength >= 5 ? 'lime' : numberLength === 4 ? 'orange' : 'gray';
-                        element.appendChild(newSpan);
+            try {
+                if (!element.classList.contains('modified')) {
+                    element.classList.remove('d-none');
+                    const newText = element.getAttribute('data-bs-original-title');
+                    console.log('Processing element:', newText);
+
+                    if (newText) {
+                        const textToAppend = newText.match(/\|\s*(\d+)/);
+                        if (textToAppend) {
+                            const newSpan = document.createElement('span');
+                            newSpan.textContent = "  | " + textToAppend[1];
+                            const numberLength = textToAppend[1].length;
+                            newSpan.style.color = numberLength >= 5 ? 'lime' : numberLength === 4 ? 'orange' : 'gray';
+                            element.appendChild(newSpan);
+                        }
                     }
+                    element.style.width = 'auto';
+                    element.classList.add('modified'); // Mark the element as modified
                 }
-                element.style.width = 'auto';
-                element.classList.add('modified'); // Mark the element as modified
+            } catch (error) {
+                console.error('Error processing element:', error);
             }
         });
     }
     
     function checkAndModifySellCountElements() {
-        if (document.querySelectorAll('.sell-count').length > 0) {
+        const elements = document.querySelectorAll('.sell-count');
+        if (elements.length > 0) {
             console.log('Elements found, modifying...');
             modifySellCountElements();
         } else {
