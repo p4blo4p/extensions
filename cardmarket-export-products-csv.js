082d2db0eba1e7a8fc9e79eef16e1797c77447d5
p4blo4p <87369479+p4blo4p@users.noreply.github.com>
2025-11-17 11:13:13 +0100

    Update cardmarket-export-products-csv.js

--- cardmarket-export-products-csv.js
+++ cardmarket-export-products-csv.js
@@ -2,11 +2,12 @@
 // ==UserScript==
 // @name         Cardmarket Price History Extractor
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.6
 // @description  Extract data, store history, and export CSV from Cardmarket
 // @author       You
 // @match        https://www.cardmarket.com/*/*/Products/*
 // @grant        none
+// @icon         https://www.cardmarket.com/favicon.ico
 // ==/UserScript==
 
 (function() {
